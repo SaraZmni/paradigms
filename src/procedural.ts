97310d32b21efbbaa16f4abd707d7f6b1ab0cada
Emma <Emma@fanap.ir>
2024-12-29 12:18:32 +0330

    complete procedural method

--- src/procedural.ts
+++ src/procedural.ts
@@ -3,7 +3,7 @@ type Card = {
   name: string;
   winingNumbers: number[];
   numbers: number[];
-}
+};
 
 export function parseLine(numStr: string): number[] {
   const strArray = numStr.split(" ");
@@ -17,30 +17,71 @@ export function parseLine(numStr: string): number[] {
   return result;
 }
 
+function readFile(){
+  return Deno.readTextFileSync('./input');
+}
+
+function parseData(file:string) {
+ 
+    const cards: Card[] = [];
+
+    const lines = file.split('\n');
+
+    for (const line of lines) {
+      if (line === '') continue;
+      
+      const [name, rest] = line.split(':');
+      const [leftNums, rightNums] = rest.split('|');
+
+      const winingNumbers = parseLine(leftNums);
+      const numbers = parseLine(rightNums);
+      
+      cards.push({
+        name,
+        winingNumbers,
+        numbers
+      });
+    }
+
+  return cards
+}
+
+function calculatePoint(card:Card) {
+  let cardPoint = 0;
+  for (const num of card.numbers) {
+    if (card.winingNumbers.includes(num)) {
+      if (cardPoint === 0) cardPoint = 1;
+      else cardPoint *= 2;
+    }
+  } 
+  return cardPoint
+}
+
+function calculateSum(cards:Card[]) {
+  let sumPoints = 0;
+
+    for (const card of cards) {
+     let cardPoint = calculatePoint(card)   
+        sumPoints += cardPoint; 
+    }
+  return sumPoints
+}
+
+function print(sumPoints:number){
+  console.log(sumPoints)
+}
+
 export function solution(): void {
-  // Read file
-  const file = Deno.readTextFileSync('./input');
-  const cards: Card[] = [];
-
-  const lines = file.split('\n');
-
-  for (const line of lines) {
-    if (line === '') continue;
+  try {
+   
+    const file = readFile()
+    let cards = parseData(file)    
+    let sumPoints = calculateSum(cards)
+    print(sumPoints)
     
-    const [name, rest] = line.split(':');
-    const [leftNums, rightNums] = rest.split('|');
-
-    const winingNumbers = parseLine(leftNums);
-    const numbers = parseLine(rightNums);
-    
-    cards.push({
-      name,
-      winingNumbers,
-      numbers
-    });
+  } catch (error) {
+    console.error('Error:', error);
   }
-
-  console.log(cards);
 }
 
 solution();
